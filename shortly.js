6688c863ad015e3017e58753612850a8f16410d7
Hack Reactor Students <accounts@hackreactor.com>
2015-06-08 14:42:24 -0700

    created logout view and added GET requests for login and signup views

--- shortly.js
+++ shortly.js
@@ -23,24 +23,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+app.get('/',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links',
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+app.post('/links',
 function(req, res) {
   var uri = req.body.url;
 
@@ -78,8 +78,23 @@ function(req, res) {
 // Write your authentication routes here
 /************************************************************/
 
+// sign-up
+app.get('/signup',
+  function(req, res){
+  res.render('signup');
+})
 
+// login
+app.get('/login',
+function(req, res) {
+  res.render('login');
+});
 
+// logout
+app.get('/logout',
+function(req, res) {
+  res.render('logout');
+});
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
 // assume the route is a short code and try and handle it here.
